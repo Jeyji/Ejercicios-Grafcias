29f94df775f3e62a95047e7192c3b2c669ffbd5d
JHOJAN <JHOJAN@TOSHIBA>
2017-03-07 01:20:23 -0500

    Punto Mover ladrillos

---
+++ POOEducacion/src/Ladrillos/Main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ladrillos;
+import Ladrillos.MoverLa.*;
+/**
+ *
+ * @author JHOJAN
+ */
+public class Main{
+public static void main (String[] args) {
+    MoverLa moverLa= new MoverLa();
+    moverLa.avance();
+    moverLa.turnrigth();
+    moverLa.avance();
+    moverLa.turnleft();
+    moverLa.avance();
+    moverLa.turnrigth();
+    moverLa.avance();
+    moverLa.turnleft();
+    moverLa.avance();
+    moverLa.turnl();
+    moverLa.avance();
+    moverLa.turnl();
+    moverLa.avance();
+    moverLa.gto();
+    moverLa.avance1();
+    moverLa.m();
+    moverLa.turnr();
+    moverLa.avance2();
+    moverLa.av();
+    moverLa.avance1();
+    moverLa.turnl();
+    moverLa.turnl();
+    moverLa.m();
+    moverLa.turnl();
+    moverLa.avance2();
+    moverLa.turnr();
+    moverLa.m();
+    moverLa.m();
+    moverLa.m();
+    moverLa.turnl();
+    moverLa.m();
+}
+}
